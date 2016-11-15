a9d906845ab9a5a03bd908b38bc0d07ca7e5d8b3
Anarky22 <goawaynowplease22@gmail.com>
2016-11-15 14:02:45 -0500

    Update Project2.cpp

--- Project2.cpp
+++ Project2.cpp
@@ -1,28 +1,30 @@
-/*#define trigPin 7
-#define led 13
+/*#define led 13
 #define led2 12
 #define led3 11
 #define led4 10
 #define led5 9
 #define led6 8
-#define buzzer 3
-*/
-#define echoPin 9;
-int sound = 0;
+#define buzzer 3*/
 
+unsigned long echo = 0;
+int ultraSoundSignal = 9; // Ultrasound signal pin
+unsigned long ultrasoundValue = 0;
+
+int frequency = 0;
 
 void setup() {
-  Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  /*pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);*/
-  pinMode(buzzer, OUTPUT);
- 
+
+  Serial.begin (9600);
+
+  /*pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(buzzer, OUTPUT);*/
+
+  pinMode(ultraSoundSignal,OUTPUT);
 }
 
 void loop() {
